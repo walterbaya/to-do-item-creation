60d4305057c21b2eab69001356af55bb56834f91
walterbaya <walterbaya@yahoo.com>
2022-02-21 18:39:39 -0300

    database is been configured

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+sudo mysql -e "DROP DATABASE IF EXISTS todo /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 sudo mysql -e "CREATE DATABASE todo /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 sudo mysql -e "USE todo;"
-sudo mysql -e "CREATE USER IF NOT EXISTS 'walter' @localhost IDENTIFIED BY '1234';"
-sudo mysql -e "GRANT ALL PRIVILEGES ON *todo* TO 'walter'@'localhost';"
+sudo mysql -e "CREATE USER IF NOT EXISTS 'creator'@'localhost' IDENTIFIED BY 'alfa1234';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON todo.* TO 'creator'@'localhost';"
 
 mvn spring-boot:run
