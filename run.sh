ebeb0f69fa24d501bdf9cb02c5da2b90ddcfe9c8
walterbaya <walterbaya@yahoo.com>
2022-02-21 16:16:02 -0300

    we add a test database

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+sudo mysql -e "CREATE DATABASE todo /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+sudo mysql -e "USE todo;"
+sudo mysql -e "CREATE USER IF NOT EXISTS 'walter' @localhost IDENTIFIED BY '1234';"
+sudo mysql -e "GRANT ALL PRIVILEGES ON *todo* TO 'walter'@'localhost';"
+
 mvn spring-boot:run
