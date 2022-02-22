343965327354abab9d1d9fa62333951f01f8366c
walterbaya <walterbaya@yahoo.com>
2022-02-21 21:23:58 -0300

    post its been tested using postman

--- src/main/java/com/example/todoapplication/model/Item.java
+++ src/main/java/com/example/todoapplication/model/Item.java
@@ -1,11 +1,13 @@
 package com.example.todoapplication.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence. *;
 import lombok.Data;
+
 import org.springframework.data.annotation.Id;
 
 @Data
 @Entity
+@Table(name= "item")
 public class Item {
 
     @Id
@@ -15,4 +17,12 @@ public class Item {
 
     private Boolean completed;
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @jakarta.persistence.Id
+    public Long getId() {
+        return id;
+    }
 }
