24748a046a45ab43394ea7d66fc3c96b950d7845
walterbaya <walterbaya@yahoo.com>
2022-02-22 13:47:55 -0300

    form in item is added

--- frontend/todo/src/components/TodoItem.vue
+++ frontend/todo/src/components/TodoItem.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>{{ info }}</div>
-  <div class="bg-dark">Hello this is an Item</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <form class="form-inline">
+          <div class="custom-control custom-checkbox my-1 mr-sm-2">
+            <input type="checkbox" class="custom-control-input">
+            <label class="custom-control-label mx-5" for="customControlInline">{{text}}</label>
+            <button type="submit" class="btn btn-outline-primary btn-sm my-1">Edit</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -29,8 +40,8 @@ export default {
   data() {
     return {
       info: null,
-      circle: "circle",
-      cross: "cross"
+      text: "Buy Groceries",
+      checked: false
     }
   },
 }
